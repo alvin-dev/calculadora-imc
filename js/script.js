27ff0eedb17eaa18f1f6f8fcac7ac0751f027c09
Alvaro <alvinweeb@gmail.com>
2019-05-13 18:12:30 -0300

    projeto calculadora imc

---
+++ js/script.js
@@ -0,0 +1,43 @@
+
+// peso / altura * altura 
+
+function calculaIMC(peso, altura) {
+    var alturaMetros = altura / 100;
+    var resultadoIMC = peso / (alturaMetros * alturaMetros);
+
+    return resultadoIMC;
+}
+
+function feedbackIMC(imc) {
+    if (imc < 18.5) {
+        return 'Magreza!'
+
+    } else if (imc >= 18.5 && imc <=29.99) {
+        return 'Peso Normal!'
+
+    }else if (imc >= 30 && imc <=34.99) {
+        return 'Obesidade I!'
+
+    }else {
+        return 'Obesidade II (severa)'
+    }
+}
+
+var feedback = feedbackIMC(calculaIMC);
+var calculadora = document.querySelector('#calculadora');
+
+var campoPeso = document.querySelector('#peso');
+var campoAltura = document.querySelector('#altura');
+
+var resultado = document.querySelector('#resultado');
+
+function manipulaInput(){
+    //validação - se o campo estiver preenchido
+    if (campoPeso.value != "" && campoAltura.value != "") {
+        
+        var imc = calculaIMC(campoPeso.value, campoAltura.value);
+        var feedback = feedbackIMC(imc);
+
+        resultado.innerHTML = `<h3>IMC: ${imc.toFixed(2)} <br> ${feedback}</h3>`;
+    }
+}
